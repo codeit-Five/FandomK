3634b3ea41dcc7438f9c0b65ed792583b808aa1d
jiyun0522 <wnlemforhs24@gmail.com>
2025-10-04 12:24:58 +0900

    ✨ Feat: 지현 - 라디오 버튼을 포함한 공통 OptionCard 컴포넌트 구현

---
+++ src/components/OptionCard/OptionCard.jsx
@@ -0,0 +1,52 @@
+import IdolCard from '../IdolCard/IdolCard';
+import creditIcon from '../../assets/image/icons/ic_credit.svg';
+import './OptionCard.scss';
+
+export default function OptionCard({
+  variant,
+  isRadioDisplay,
+  radioInfo,
+  creditValue,
+  idolCardInfo,
+}) {
+  const { id, name, selected, setSelected } = radioInfo || {};
+  const { imgUrl, rankInfo, profileInfo } = idolCardInfo || {};
+
+  const variantClass = `optionCard${variant.charAt(0).toUpperCase()}${variant.slice(1)}`;
+  const isSelected = selected === id;
+
+  return (
+    <div
+      className={`${variantClass} ${isSelected && variant === 'recharge' ? 'selected' : ''}`.trim()}
+    >
+      {variant === 'recharge' && (
+        <div className="creditBox">
+          <img src={creditIcon} alt="Credit Icon" />
+          <span>{creditValue}</span>
+        </div>
+      )}
+
+      {variant === 'chart' && (
+        <IdolCard
+          variant={variant}
+          imgUrl={imgUrl}
+          rankInfo={rankInfo}
+          profileInfo={profileInfo}
+          isModal
+          isSelected={isSelected}
+        />
+      )}
+
+      {isRadioDisplay && (
+        <input
+          type="radio"
+          id={`radio${id}`}
+          name={name}
+          value={id}
+          // checked={selected === id}
+          onChange={() => setSelected(id)}
+        />
+      )}
+    </div>
+  );
+}
